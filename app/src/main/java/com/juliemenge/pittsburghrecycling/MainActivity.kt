79ae338506d76dcce4d16ccaeb2e5b6a9334f758
Julie Menge <juliemenge@gmail.com>
2017-11-19 21:46:29 -0500

    UI update and hide next recycling date if null

--- app/src/main/java/com/juliemenge/pittsburghrecycling/MainActivity.kt
+++ app/src/main/java/com/juliemenge/pittsburghrecycling/MainActivity.kt
@@ -2,19 +2,13 @@ package com.juliemenge.pittsburghrecycling
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import org.jetbrains.anko.*
-import java.time.LocalDate
-import java.time.chrono.ChronoLocalDate
-import java.time.chrono.Chronology
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,17 +18,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var selectedRecyclingDates = arrayOf<String>()
         //list of recycling dates
         val recyclingDatesCentralEastern = arrayOf("11-27-2017", "12-11-2017", "12-26-2017")
         val recyclingDatesNorthernSouthern = arrayOf("11-20-2017", "12-04-2017", "12-18-2017")
+        //array to store dates based on chosen neighborhood
+        var selectedRecyclingDates = arrayOf<String>()
 
         //get today's date
         val current = LocalDateTime.now() //current date
         val formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy") //how you want the date to display
         val formattedCurrentDate = current.format(formatter) //current date displayed the way you want it
 
-        //spinner to store which neighborhoods
+        //spinner to select neighborhood
         val aa: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, neighborhoods)
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         neighborhoodSpinner.adapter = aa
@@ -48,15 +43,17 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 val selectedItem = neighborhoodSpinner.selectedItem.toString()
                 if (selectedItem == "Central/Eastern") {
-                    toast("You picked Central/Eastern!")
-                    selectedRecyclingDates = recyclingDatesCentralEastern
 
-                    for (date in selectedRecyclingDates) {
-                        println(date)
-                    }
+                    selectedRecyclingDates = recyclingDatesCentralEastern //set correct list of recycling dates
 
+                    //find and set next recycling date
+                    //TODO: if there is no next date - aka, it is null, display "see ya next year" or something
                     val nextRecyclingDate = selectedRecyclingDates.firstOrNull { it > formattedCurrentDate }
-                    nextRecyclingDayTextView.text = nextRecyclingDate //set the next recycling day text
+
+                    if (nextRecyclingDate == null) {
+                        nextRecyclingDayTextView.visibility = View.INVISIBLE
+                        nextRecyclingDayLabel.visibility = View.INVISIBLE
+                    } else nextRecyclingDayTextView.text = nextRecyclingDate //set the next recycling day text
 
                     //check if today is recycling day
                     if (selectedRecyclingDates.contains(formattedCurrentDate)) {
@@ -64,42 +61,22 @@ class MainActivity : AppCompatActivity() {
                     } else recyclingAnswerTextView.text = "NO"
 
                 } else {
-                    toast("You picked Northern/Southern!")
-                    selectedRecyclingDates = recyclingDatesNorthernSouthern
 
-                    for (date in selectedRecyclingDates) {
-                        println(date)
-                    }
+                    selectedRecyclingDates = recyclingDatesNorthernSouthern //set correct list of recycling dates
 
+                    //find and set next recycling date
                     val nextRecyclingDate = selectedRecyclingDates.firstOrNull { it > formattedCurrentDate }
-                    nextRecyclingDayTextView.text = nextRecyclingDate //set the next recycling day text
+
+                    if (nextRecyclingDate == null) {
+                        nextRecyclingDayTextView.visibility = View.INVISIBLE
+                        nextRecyclingDayLabel.visibility = View.INVISIBLE
+                    } else nextRecyclingDayTextView.text = nextRecyclingDate //set the next recycling day text
 
                     //check if today is recycling day
                     if (selectedRecyclingDates.contains(formattedCurrentDate)) recyclingAnswerTextView.text = "YES" else recyclingAnswerTextView.text = "NO"
                 }
             }
         }
-
-
-        //TODO: figure out how to set the next recycling date without hard coding it in - compare today's date
-        //TODO: to each item in the array, if today's date is greater than array date, compare today's date to next date, if next date in array is
-        //TODO: greater than today's date, set that to be next recycling date
-
-        //set the next recycling date
-        //val nextRecyclingDate = selectedRecyclingDates.firstOrNull { it > formattedCurrentDate }
-
-        //val nextRecyclingDate = "11-27-2017"
-
-
-//        fun dateCompare (dateOne: String, dateTwo: String) : Boolean {
-//            if (dateOne > dateTwo) {
-//                return true
-//            } else return false
-//        }
-//
-//        val answer = dateCompare("12-10-17", "12-08-17")
-
-
 
     }
 }
